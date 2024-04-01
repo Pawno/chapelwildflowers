3a1bcceb27be804e446073681bbaf034b17b4a17
Norris Duncan <norris.duncan@gmail.com>
2024-03-31 21:10:58 -0400

    Added some admin models and a bigger slideshow above the fold

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from website.models import BlogPost, EmailSubscriber
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(EmailSubscriber)
+class CourseAdmin(admin.ModelAdmin):
+    pass
--- website/models.py
+++ website/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class BlogPost(models.Model):
+    timestamp = models.timestamp(auto_now_add=True)
+    author = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
+
+class EmailSubscriber(models.Model):
+    email = models.EmailField(max_length=100)
