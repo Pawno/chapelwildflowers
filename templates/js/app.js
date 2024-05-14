42262e6e6ab1c6a77012f67f6778f10e165f05db
Norris Duncan <norris.duncan@gmail.com>
2024-05-14 09:11:48 +0000

    Made it work good

--- templates/js/app.js
+++ templates/js/app.js
@@ -1,12 +1,14 @@
-(function($, document, window){
-	
-	$(document).ready(function(){
+
+
+(function ($, document, window) {
+	$(document).ready(function () {
+		console.log("READY")
 
 		// Cloning main navigation for mobile menu
 		$(".mobile-navigation").append($(".main-navigation .menu").clone());
 
 		// Mobile menu toggle 
-		$(".menu-toggle").click(function(){
+		$(".menu-toggle").click(function () {
 			$(".mobile-navigation").slideToggle();
 		});
 
@@ -18,48 +20,71 @@
 		var map = $(".map");
 		var latitude = map.data("latitude");
 		var longitude = map.data("longitude");
-		if( map.length ){
-			
+		if (map.length) {
+
 			map.gmap3({
-				map:{
-					options:{
-						center: [latitude,longitude],
+				map: {
+					options: {
+						center: [latitude, longitude],
 						zoom: 15,
 						scrollwheel: false
 					}
 				},
-				marker:{
-					latLng: [latitude,longitude],
-					options:{
-			          icon: "images/map-pin.png"
-			        }
+				marker: {
+					latLng: [latitude, longitude],
+					options: {
+						icon: "images/map-pin.png"
+					}
 				}
 			});
-			
+
 		}
 
 		// var waypoint = new Waypoint({
-		  var $section =  $(".fullwidth-block");
-		  $section.each(function(){
-		  	$(this).waypoint(function(){
-		  		$("a[href=#"+this.element.id+"]" ).parent("li").toggleClass("current-menu-item").siblings().removeClass("current-menu-item");
-		  	},{
-		  		offset: 200
-		  	});
-		  });
-		  
+		var $section = $(".linkedSection");
+		console.log("$section" + $section)
+		$section.each(function () {
+			console.log("links")
+			console.log(this.toString())
+			if (this.id == "slideshow") {
+				$(this).waypoint(function () {
+				$("#links").children().removeClass("current-menu-item");
+				}, {
+					offset: 0
+				});
+			} else {
+				$(this).waypoint(function () {
+					console.log("waypoint" + this.element.id)
+					$("#" + this.element.id + "Link").toggleClass("current-menu-item").siblings().removeClass("current-menu-item");
+				}, {
+					offset: 0
+				});
+			}
+		});
+
 
 	});
 
 	$(window).scroll(function () {
-        if ($(this).scrollTop() > 1) {
-            $('.site-header').addClass("sticky");
-        } else {
-            $('.site-header').removeClass("sticky");
-        }
-    }); 
 
-	$(window).load(function(){
+		// console.log("SCROLLLLLLLLLLING")
+		if ($(this).scrollTop() > 1) {
+			$('.siteHeader').addClass("sticky");
+		} else {
+			$('.siteHeader').removeClass("sticky");
+		}
+
+		// // Check if the element is visible
+		// if ($('#map').is(':visible')) {
+		// 	// Element is visible
+		// 	console.log('Element is visible');
+		// } else {
+		// 	// Element is not visible
+		// 	console.log('Element is not visible');
+		// }
+	});
+
+	$(window).load(function () {
 
 	});
 
